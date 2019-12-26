56bd5290667f7a1e6c2a45dc452094a4eeaa7446
zhenjie.xuan <zhenjie.xuan@corp.to8to.com>
2019-12-26 15:18:46 +0800

    [add]first commit

---
+++ src/main/java/com/xuanzjie/filemanage/FileManageApplication.java
@@ -0,0 +1,15 @@
+package com.xuanzjie.filemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class FileManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xuanzjie/filemanage/FileManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuanzjie.filemanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
